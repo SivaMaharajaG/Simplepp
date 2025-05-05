412fb733eb71da29b7affe660e59b6d56d654524
SivaMaharajaG <sivamaharaja777@gmail.com>
2025-05-05 22:45:56 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,12 +1,12 @@
 import streamlit as st
 st.title("SivaKing2475")
 st.sidebar.title("Give us your valid Info")
-st.sidebar.text_input("Enter your Email ","Type here ...")
+st.sidebar.text_input("Enter your Email ")
 if(st.sidebar.button("Register")):
     st.sidebar.success("Your email verified")
-st.text_input("Enter your name ","Type here ...")
+st.text_input("Enter your name ")
 st.date_input("Enter your DOB ")
-st.text_input("Enter your Mobile Number","Type here ...")
+st.text_input("Enter your Mobile Number")
 st.radio("Select your Gender",["Male","Female","Others"])
 if(st.button("Submit")):
     st.write("Thank you for yor registration")
